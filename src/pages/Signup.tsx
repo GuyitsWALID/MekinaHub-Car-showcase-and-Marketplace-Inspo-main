b380c071bf2d069acd40a6ffce5affd3d188735b
GuyitsWALID <walidmurad65@gmail.com>
2025-04-01 16:29:05 +0300

    just a test commit

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -19,7 +19,7 @@ const Signup = () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'github',
       options: {
-        redirectTo: `${window.location.origin}/auth/v1/callback` // Change this to your desired redirect page
+        redirectTo: `${window.location.origin}/showroom` // Change this to your desired redirect page
       }
     });
   
